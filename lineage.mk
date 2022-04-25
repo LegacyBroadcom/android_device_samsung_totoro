ab3fc5e721f9f34b0a9ee52819f9842605b4be11
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-04-25 18:29:33 +0200

    cm: Rebrand to LineageOS

--- lineage.mk
+++ lineage.mk
@@ -10,7 +10,7 @@ TARGET_SCREEN_WIDTH := 240
 $(call inherit-product, vendor/cm/config/mini.mk)
 
 # Overrides
-PRODUCT_NAME := cm_totoro
+PRODUCT_NAME := lineage_totoro
 PRODUCT_DEVICE := totoro
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-S5360

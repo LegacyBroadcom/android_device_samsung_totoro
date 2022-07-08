af0059623560b55b030f01fe99154b5e1b34f35c
Tri <triplaysyt@gmail.com>
2022-07-08 16:07:30 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -14,7 +14,4 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),totoro)
-    include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
-
+include $(call all-makefiles-under,$(LOCAL_PATH))
